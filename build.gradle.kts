85c6e2f1f309d34f36317ab55f30673ad28aafe7
WeiHung <wei_hung.chin@daimler.com>
2021-02-06 15:53:42 +0800

    Add dgs codegen dependency and codegen trigger config

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
+	id("com.netflix.dgs.codegen") version "4.0.12"
 }
 
 group = "com.example"
@@ -23,6 +24,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+@OptIn(kotlin.ExperimentalStdlibApi::class)
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+	generateClient = true
+	packageName = "com.example.kotlinDgsExample.generated"
 }
 
 tasks.withType<KotlinCompile> {
