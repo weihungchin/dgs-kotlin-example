ada2706cb083c17efbd16f5e7dd12ad0a4ffa6dd
WeiHung <wei_hung.chin@daimler.com>
2021-02-06 15:15:22 +0800

    Init spring boot with spring web

---
+++ src/main/kotlin/com/example/kotlinDgsExample/KotlinDgsExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinDgsExample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDgsExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDgsExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinDgsExample/KotlinDgsExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinDgsExample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDgsExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
