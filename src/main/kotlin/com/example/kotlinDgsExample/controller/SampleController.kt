8df06de9fd24ec5a460eab50f6cbd172ea24d4ff
WeiHung <wei_hung.chin@daimler.com>
2021-03-23 21:39:26 +0800

    Add cors

--- src/main/kotlin/com/example/kotlinDgsExample/controller/SampleController.kt
+++ src/main/kotlin/com/example/kotlinDgsExample/controller/SampleController.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinDgsExample.controller
 
 import com.example.kotlinDgsExample.service.AccountService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class SampleController(private val accountService: AccountService) {
 
+    @CrossOrigin()
     @RequestMapping("/api/greet")
-    fun helloWorld(@RequestParam name: String): String {
-        return "hello $name"
+    fun helloWorld(): String {
+        return "hello cors enabled"
     }
 
     @RequestMapping("/api/account")
